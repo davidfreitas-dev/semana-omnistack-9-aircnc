b32320303fb5c1d0b563853c0e356738a2883b24
David Freitas <davidfreitas@MacBook-Pro-de-David.local>
2021-10-24 11:54:26 -0300

    Dia 3 Concluído

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
+const cors = require ('cors');
+const path = require ('path');
+
 const routes = require ('./routes');
 
 const app = express();
@@ -13,7 +16,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistackcluster.1zqh0.mongo
 // req.params = Acessar os parametros da rota (para edição, delete)
 // req.body = Acessar o corpo da requisição (para criação, edição)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
